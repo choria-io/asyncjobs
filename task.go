c3a9f50965a990131c546ec3f56f706123bfd812
R.I.Pienaar <rip@devco.net>
2022-01-27 18:55:05 +0100

    (misc) add some task documentation
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- task.go
+++ task.go
@@ -12,40 +12,64 @@ import (
 	"github.com/segmentio/ksuid"
 )
 
+// TaskState indicates the current state a task is in
 type TaskState string
 
 const (
-	TaskStateUnknown    TaskState = "unknown"
-	TaskStateNew        TaskState = "new"
-	TaskStateActive     TaskState = "active"
-	TaskStateRetry      TaskState = "retry"
-	TaskStateExpired    TaskState = "expired"
-	TaskStateCompleted  TaskState = "complete"
+	// TaskStateUnknown is for tasks that do not have a state set
+	TaskStateUnknown TaskState = ""
+	// TaskStateNew newly created tasks that have not been handled yet
+	TaskStateNew TaskState = "new"
+	// TaskStateActive tasks that are currently being handled
+	TaskStateActive TaskState = "active"
+	// TaskStateRetry tasks that previously failed and are waiting retry
+	TaskStateRetry TaskState = "retry"
+	// TaskStateExpired tasks that reached their deadline
+	TaskStateExpired TaskState = "expired"
+	// TaskStateCompleted tasks that are completed
+	TaskStateCompleted TaskState = "complete"
+	// TaskStateQueueError tasks that could not have their associated Work Queue item created
 	TaskStateQueueError TaskState = "queue_error"
 )
 
+// Task represents a job item that handlers will execute
 type Task struct {
-	ID          string      `json:"id"`
-	Type        string      `json:"type"`
-	Queue       string      `json:"queue"`
-	Payload     []byte      `json:"payload"`
-	Deadline    *time.Time  `json:"deadline,omitempty"`
-	Result      *TaskResult `json:"result,omitempty"`
-	State       TaskState   `json:"state"`
-	CreatedAt   time.Time   `json:"created"`
-	LastTriedAt *time.Time  `json:"tried,omitempty"`
-	Tries       int         `json:"tries"`
-	LastErr     string      `json:"last_err,omitempty"`
+	// ID is a k-sortable unique ID for the task
+	ID string `json:"id"`
+	// Type is a free form string that can later be used as a routing key to send tasks to handlers
+	Type string `json:"type"`
+	// Queue is the name of the queue the task was enqueued with, set only during the enqueue operation else empty
+	Queue string `json:"queue"`
+	// Payload is a JSON representation of the associated work
+	Payload []byte `json:"payload"`
+	// Deadline is a cut-off time for the job to complete, should a job be scheduled after this time it will fail.
+	// In-Flight jobs are allowed to continue past this time. Only starting handlers are impacted by this deadline.
+	Deadline *time.Time `json:"deadline,omitempty"`
+	// Result is the outcome of the job, only set for successful jobs
+	Result *TaskResult `json:"result,omitempty"`
+	// State is the most recent recorded state the job is in
+	State TaskState `json:"state"`
+	// CreatedAt is the time the job was created in UTC timezone
+	CreatedAt time.Time `json:"created"`
+	// LastTriedAt is a time stamp for when the job was last handed to a handler
+	LastTriedAt *time.Time `json:"tried,omitempty"`
+	// Tries is how many times the job was handled
+	Tries int `json:"tries"`
+	// LastErr is the most recent handling error if any
+	LastErr string `json:"last_err,omitempty"`
 
 	storageOptions interface{}
 	mu             sync.Mutex
 }
 
+// TaskResult is the result of task execution, this will only be set for successfully processed jobs
 type TaskResult struct {
 	Payload     []byte    `json:"payload"`
 	CompletedAt time.Time `json:"completed"`
 }
 
+// NewTask creates a new task of taskType that can later be used to route tasks to handlers.
+// The task will carry a JSON encoded representation of payload.
 func NewTask(taskType string, payload interface{}, opts ...TaskOpt) (*Task, error) {
 	p, err := json.Marshal(payload)
 	if err != nil {
@@ -75,6 +99,7 @@ func NewTask(taskType string, payload interface{}, opts ...TaskOpt) (*Task, erro
 	return t, nil
 }
 
+// TaskOpt configures Tasks made using NewTask()
 type TaskOpt func(*Task) error
 
 // TaskDeadline sets an absolute time after which the task should not be handled
