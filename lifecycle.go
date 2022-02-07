d8e4aed97552e0069a2ab200cada2a43aa82eeb1
R.I.Pienaar <rip@devco.net>
2022-02-07 11:17:09 +0100

    (#15) minor updates to lifecycle events
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- lifecycle.go
+++ lifecycle.go
@@ -12,22 +12,31 @@ import (
 	"github.com/segmentio/ksuid"
 )
 
+// BaseEvent is present in all event types and can be used to detect the type
 type BaseEvent struct {
-	EventID   string `json:"event_id"`
-	EventType string `json:"type"`
-	TimeStamp int64  `json:"timestamp"`
+	EventID   string    `json:"event_id"`
+	EventType string    `json:"type"`
+	TimeStamp time.Time `json:"timestamp"`
 }
 
+// TaskStateChangeEvent notifies that a significant change occurred in a Task
 type TaskStateChangeEvent struct {
-	TaskID   string        `json:"task_id"`
-	State    TaskState     `json:"state"`
-	Tries    int           `json:"tries"`
-	Queue    string        `json:"queue"`
-	TaskType string        `json:"task_type"`
-	LastErr  string        `json:"last_error,omitempty"`
-	Age      time.Duration `json:"task_age,omitempty"`
-
 	BaseEvent
+
+	// TaskID is the ID of the task, use with LoadTaskByID() to access the task
+	TaskID string `json:"task_id"`
+	// State is the new state of the Task
+	State TaskState `json:"state"`
+	// Tries is how many times the Task has been processed
+	Tries int `json:"tries"`
+	// Queue is the queue the task is in, can be empty
+	Queue string `json:"queue,omitempty"`
+	// TaskType is the task routing type
+	TaskType string `json:"task_type"`
+	// LstErr is the error that caused a task to change state for error state changes
+	LastErr string `json:"last_error,omitempty"`
+	// Age is the time since the task was created in milliseconds
+	Age time.Duration `json:"task_age,omitempty"`
 }
 
 const (
@@ -35,6 +44,7 @@ const (
 	TaskStateChangeEventType = "io.choria.asyncjobs.v1.task_state"
 )
 
+// ParseEventJSON parses event bytes returning the parsed Event and its event type
 func ParseEventJSON(event []byte) (interface{}, string, error) {
 	var base BaseEvent
 	err := json.Unmarshal(event, &base)
@@ -72,13 +82,13 @@ func NewTaskStateChangeEvent(t *Task) (*TaskStateChangeEvent, error) {
 		LastErr:  t.LastErr,
 		BaseEvent: BaseEvent{
 			EventID:   eid.String(),
-			TimeStamp: eid.Time().UnixNano(),
+			TimeStamp: eid.Time().UTC(),
 			EventType: TaskStateChangeEventType,
 		},
 	}
 
 	if !t.CreatedAt.IsZero() {
-		e.Age = time.Since(t.CreatedAt)
+		e.Age = time.Since(t.CreatedAt.Round(time.Millisecond))
 	}
 
 	return e, nil
