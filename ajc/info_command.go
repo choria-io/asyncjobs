1d1db442c20bff9295857f938c69daf615f82f92
R.I.Pienaar <rip@devco.net>
2022-05-05 13:27:54 +0200

    (#67) add some init options to ajc info
    This is not the full storage management feature I have in mind
    but enough to get the most annoying niggles sorted now
    
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- ajc/info_command.go
+++ ajc/info_command.go
@@ -6,16 +6,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/choria-io/asyncjobs"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-func configureInfoCommand(app *kingpin.Application) {
-	app.Command("info", "Shows general Task and Queue information").Action(infoAction)
+type infoCommand struct {
+	replicas  uint
+	memory    bool
+	retention time.Duration
 }
 
-func infoAction(_ *kingpin.ParseContext) error {
-	err := prepare()
+func configureInfoCommand(app *kingpin.Application) {
+	c := &infoCommand{}
+
+	info := app.Command("info", "Shows general Task and Queue information").Action(c.infoAction)
+	info.Flag("replica", "When initializing, do so with this many replicas").Short('R').Default("1").UintVar(&c.replicas)
+	info.Flag("memory", "When initializing, do so with memory storage").BoolVar(&c.memory)
+	info.Flag("retention", "When initializing, sets how long Tasks are kept").DurationVar(&c.retention)
+}
+
+func (c *infoCommand) infoAction(_ *kingpin.ParseContext) error {
+	err := prepare(asyncjobs.MemoryStorage(), asyncjobs.StoreReplicas(c.replicas), asyncjobs.TaskRetention(c.retention))
 	if err != nil {
 		return err
 	}
