20038a1dbf72671279db647e458168004372827a
R.I.Pienaar <rip@devco.net>
2023-01-11 22:35:52 +0100

    (#106) Fix storage choice when doing init via info
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- ajc/info_command.go
+++ ajc/info_command.go
@@ -23,12 +23,17 @@ func configureInfoCommand(app *fisk.Application) {
 
 	info := app.Command("info", "Shows general Task and Queue information").Action(c.infoAction)
 	info.Flag("replica", "When initializing, do so with this many replicas").Short('R').Default("1").UintVar(&c.replicas)
-	info.Flag("memory", "When initializing, do so with memory storage").BoolVar(&c.memory)
+	info.Flag("memory", "When initializing, do so with memory storage").UnNegatableBoolVar(&c.memory)
 	info.Flag("retention", "When initializing, sets how long Tasks are kept").DurationVar(&c.retention)
 }
 
 func (c *infoCommand) infoAction(_ *fisk.ParseContext) error {
-	err := prepare(asyncjobs.MemoryStorage(), asyncjobs.StoreReplicas(c.replicas), asyncjobs.TaskRetention(c.retention))
+	opts := []asyncjobs.ClientOpt{asyncjobs.StoreReplicas(c.replicas), asyncjobs.TaskRetention(c.retention)}
+	if c.memory {
+		opts = append(opts, asyncjobs.MemoryStorage())
+	}
+
+	err := prepare(opts...)
 	if err != nil {
 		return err
 	}
