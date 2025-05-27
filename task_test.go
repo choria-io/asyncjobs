8c444f3d2981fe2ec3eeee6c1baccdcc449ff5de
Muhammad Saboor <msaboor35@gmail.com>
2025-05-27 14:31:37 +0500

    test: add tests for custom task id

--- task_test.go
+++ task_test.go
@@ -1,13 +1,33 @@
 package asyncjobs
 
 import (
+	"context"
+	"log"
 	"time"
 
+	"github.com/nats-io/jsm.go"
+	"github.com/nats-io/nats.go"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Tasks", func() {
+	var (
+		ctx    context.Context
+		cancel context.CancelFunc
+	)
+
+	BeforeEach(func() {
+		ctx, cancel = context.WithTimeout(context.Background(), time.Minute)
+		log.SetOutput(GinkgoWriter)
+	})
+
+	AfterEach(func() { cancel() })
+
+	testQueue := func() *Queue {
+		return &Queue{Name: "ginkgo"}
+	}
+
 	Describe("NewTask", func() {
 		It("Should create a valid task with options supplied", func() {
 			p, err := NewTask("parent", nil)
@@ -47,5 +67,70 @@ var _ = Describe("Tasks", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(msg).To(HaveLen(102))
 		})
+	})
+
+	Describe("NewTaskWithCustomID", func() {
+		It("Should create a valid task with valid custom ID", func() {
+			withJetStream(func(nc *nats.Conn, mgr *jsm.Manager) {
+				CustomIDGenerator := func(t *Task) error {
+					t.ID = "my-custom-id"
+					return nil
+				}
+
+				storage, err := newJetStreamStorage(nc, retryForTesting, &defaultLogger{})
+				Expect(err).ToNot(HaveOccurred())
+
+				q := testQueue()
+				err = storage.PrepareQueue(q, 1, true)
+				Expect(err).ToNot(HaveOccurred())
+				err = storage.PrepareTasks(true, 1, time.Hour)
+				Expect(err).ToNot(HaveOccurred())
+
+				task, err := NewTask("test", nil, CustomIDGenerator)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(task.ID).ToNot(HaveLen(0))
+				Expect(task.ID).To(Equal("my-custom-id"))
+
+				err = storage.EnqueueTask(ctx, q, task)
+				Expect(err).ToNot(HaveOccurred())
+
+				t, err := storage.LoadTaskByID(task.ID)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(t.ID).To(Equal(task.ID))
+				Expect(task.ID).To(Equal("my-custom-id"))
+			})
+		})
+
+		It("Should not create a task with invalid custom ID", func() {
+			withJetStream(func(nc *nats.Conn, mgr *jsm.Manager) {
+				CustomIDGenerator := func(t *Task) error {
+					t.ID = "my.invalid.custom.id"
+					return nil
+				}
+
+				storage, err := newJetStreamStorage(nc, retryForTesting, &defaultLogger{})
+				Expect(err).ToNot(HaveOccurred())
+
+				q := testQueue()
+				err = storage.PrepareQueue(q, 1, true)
+				Expect(err).ToNot(HaveOccurred())
+				err = storage.PrepareTasks(true, 1, time.Hour)
+				Expect(err).ToNot(HaveOccurred())
+
+				task, err := NewTask("test", nil, CustomIDGenerator)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(task.ID).ToNot(HaveLen(0))
+				Expect(task.ID).To(Equal("my.invalid.custom.id"))
+
+				err = storage.EnqueueTask(ctx, q, task)
+				Expect(err).ToNot(HaveOccurred()) // TODO: test fails here because custom ID has invalid characters
+
+				t, err := storage.LoadTaskByID(task.ID)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(t.ID).To(Equal(task.ID))
+				Expect(task.ID).To(Equal("my.invalid.custom.id"))
+			})
+		})
+
 	})
 })
